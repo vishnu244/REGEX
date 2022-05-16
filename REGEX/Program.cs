0393868e79b1eddf4e2efa217add346ee967a923
vishnu244 <vishnunali244@gmail.com>
2022-05-16 15:14:41 +0530

    [Vishnu] Validating FirstName

--- REGEX/Program.cs
+++ REGEX/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using REGEX;
+
 Console.WriteLine("Welcome to User Registration Problem Using REGEX!");
+
+UserRegistration userRegistration = new UserRegistration();
+userRegistration.FirstName();
+
---
+++ REGEX/UserRegistration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace REGEX
+{
+    public class UserRegistration
+    {
+        public void FirstName()
+        {
+            Console.WriteLine("Enter First Name");
+            string Firstname = Console.ReadLine();
+            string firstname = "[A-Z]{1}[a-z]{2,}";
+            Regex regex = new Regex(firstname);
+            
+            if (regex.IsMatch(Firstname))
+            {
+                Console.Write("Name is Valid \n");
+            }
+            else
+            {
+                Console.Write("Plase Enter First Letter Capital \n");
+            }
+
+        }
+      
+    }
+}
